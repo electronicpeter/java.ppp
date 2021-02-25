b4df998f468f3fd0da86074cecb3e31f1ab6c474
peter spiessbach <gotostandby@electronicpeter.de>
2021-02-25 11:13:53 +0100

    docu

--- src/main/java/de/electronicpeter/combination/Combination.java
+++ src/main/java/de/electronicpeter/combination/Combination.java
@@ -30,10 +30,11 @@ public class Combination {
         cycles.add(firstCycle);
 
         /**
-         * [0, 4, 8, 12]
-         * [1, 5, 9, 13]
-         * [2, 6, 10, 14]
-         * [3, 7, 11, 15]
+         * [   0   1   4   9  16]
+         * [   2   3   5  10  17]
+         * [   6   7   8  11  18]
+         * [  12  13  14  15  19]
+         * [  20  21  22  23  24]
          */
 
         for (int shift = 0; shift < square.getDimension(); shift++) {
