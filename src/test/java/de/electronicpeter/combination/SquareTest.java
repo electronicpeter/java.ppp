00dbd5e61dc45fba2e1dc392af66d98ccf57e0dd
peter <peter@Peters-MBP.fritz.box>
2021-02-27 16:09:40 +0100

    square2

--- src/test/java/de/electronicpeter/combination/SquareTest.java
+++ src/test/java/de/electronicpeter/combination/SquareTest.java
@@ -7,8 +7,9 @@ import org.junit.jupiter.api.Test;
 public class SquareTest {
     @Test
     public void a() {
-        log.info(new Square(17, Square.FillAlgorithm.SQUARE).toString());
-        log.info(new Square(17, Square.FillAlgorithm.ROW).toString());
-        log.info(new Square(17, Square.FillAlgorithm.SPACED).toString());
+        log.info(new Square(53*53, Square.FillAlgorithm.SQUARE).toPrimeString());
+        // log.info(new Square(53*53, Square.FillAlgorithm.SQUARE2).toPrimeString());
+        // log.info(new Square(121, Square.FillAlgorithm.ROW).toString());
+        // log.info(new Square(17, Square.FillAlgorithm.SPACED).toString());
     }
 }
