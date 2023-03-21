8aa12f12b3a9eeebba7039492559d323696c178a
electronicpeter <github@electronicpeter.de>
2023-03-21 19:34:13 +0100

    modified deploy script

--- 00.angular.client/deploy.sh
+++ 00.angular.client/deploy.sh
@@ -1,4 +1,5 @@
 cd dist/angular.peters-perfect-permutation
 tar -cf ../../ppp.tar *
+scp ../../ppp.tar root@electronicpeter.de:/var/www/vhosts/electronicpeter.de/perfect.permutation.electronicpeter.de
+ssh root@electronicpeter.de tar -C /var/www/vhosts/electronicpeter.de/perfect.permutation.electronicpeter.de -xf /var/www/vhosts/electronicpeter.de/perfect.permutation.electronicpeter.de/ppp.tar
 cd -
-scp ppp.tar root@electronicpeter.de:/var/www/vhosts/electronicpeter.de/perfect.permutation.electronicpeter.de
